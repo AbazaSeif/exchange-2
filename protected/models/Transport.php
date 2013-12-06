dff2396cb34a67f2fb024b135812b8885b2e6749
Cheshenkov <cheshenkov@lbr.ru>
2013-12-06 10:08:07 +0300

    Init new repository. New project - Exchange

---
+++ protected/models/Transport.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * This is the model class for table "transport".
+ *
+ * The followings are the available columns in table 'transport':
+ * @property integer $id
+ * @property integer $id_1c
+ * @property integer $rate_id
+ * @property integer $start_rate
+ * @property integer $status
+ * @property integer $type
+ * @property integer $user_id
+ * @property string $location_from
+ * @property string $location_to
+ * @property string $auto_info
+ * @property string $description
+ * @property string $date_from
+ * @property string $date_to
+ * @property string $date_published
+ *
+ * The followings are the available model relations:
+ * @property Message[] $messages
+ * @property Rate[] $rates
+ * @property TransportFieldEq[] $transportFieldEqs
+ * @property UserEvent[] $userEvents
+ */
+class Transport extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'transport';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('id_1c, rate_id, start_rate, status, type, user_id', 'numerical', 'integerOnly'=>true),
+			array('location_from, location_to, auto_info, description, date_from, date_to, date_published', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, id_1c, rate_id, start_rate, status, type, user_id, location_from, location_to, auto_info, description, date_from, date_to, date_published', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'messages' => array(self::HAS_MANY, 'Message', 'transport_id'),
+			'rates' => array(self::HAS_MANY, 'Rate', 'transport_id'),
+			'transportFieldEqs' => array(self::HAS_MANY, 'TransportFieldEq', 'transport_id'),
+			'userEvents' => array(self::HAS_MANY, 'UserEvent', 'transport_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'id_1c' => 'Id 1c',
+			'rate_id' => 'Rate',
+			'start_rate' => 'Start Rate',
+			'status' => 'Status',
+			'type' => 'Type',
+			'user_id' => 'User',
+			'location_from' => 'Location From',
+			'location_to' => 'Location To',
+			'auto_info' => 'Auto Info',
+			'description' => 'Description',
+			'date_from' => 'Date From',
+			'date_to' => 'Date To',
+			'date_published' => 'Date Published',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('id_1c',$this->id_1c);
+		$criteria->compare('rate_id',$this->rate_id);
+		$criteria->compare('start_rate',$this->start_rate);
+		$criteria->compare('status',$this->status);
+		$criteria->compare('type',$this->type);
+		$criteria->compare('user_id',$this->user_id);
+		$criteria->compare('location_from',$this->location_from,true);
+		$criteria->compare('location_to',$this->location_to,true);
+		$criteria->compare('auto_info',$this->auto_info,true);
+		$criteria->compare('description',$this->description,true);
+		$criteria->compare('date_from',$this->date_from,true);
+		$criteria->compare('date_to',$this->date_to,true);
+		$criteria->compare('date_published',$this->date_published,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Transport the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
---
+++ protected/models/UserField.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * This is the model class for table "user_field".
+ *
+ * The followings are the available columns in table 'user_field':
+ * @property integer $id
+ * @property integer $user_id
+ * @property boolean $mail_transport_create_1
+ * @property boolean $mail_transport_create_2
+ * @property boolean $mail_kill_rate
+ * @property boolean $mail_deadline
+ * @property boolean $mail_before_deadline
+ * @property boolean $site_transport_create_1
+ * @property boolean $site_transport_create_2
+ * @property boolean $site_kill_rate
+ * @property boolean $site_deadline
+ * @property boolean $site_before_deadline
+ *
+ * The followings are the available model relations:
+ * @property User $user
+ */
+class UserField extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'user_field';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('user_id', 'numerical', 'integerOnly'=>true),
+			array('mail_transport_create_1, mail_transport_create_2, mail_kill_rate, mail_deadline, mail_before_deadline, site_transport_create_1, site_transport_create_2, site_kill_rate, site_deadline, site_before_deadline', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, user_id, mail_transport_create_1, mail_transport_create_2, mail_kill_rate, mail_deadline, mail_before_deadline, site_transport_create_1, site_transport_create_2, site_kill_rate, site_deadline, site_before_deadline', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'user' => array(self::BELONGS_TO, 'User', 'user_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'user_id' => 'User',
+			'mail_transport_create_1' => 'Mail Transport Create 1',
+			'mail_transport_create_2' => 'Mail Transport Create 2',
+			'mail_kill_rate' => 'Mail Kill Rate',
+			'mail_deadline' => 'Mail Deadline',
+			'mail_before_deadline' => 'Mail Before Deadline',
+			'site_transport_create_1' => 'Site Transport Create 1',
+			'site_transport_create_2' => 'Site Transport Create 2',
+			'site_kill_rate' => 'Site Kill Rate',
+			'site_deadline' => 'Site Deadline',
+			'site_before_deadline' => 'Site Before Deadline',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('user_id',$this->user_id);
+		$criteria->compare('mail_transport_create_1',$this->mail_transport_create_1);
+		$criteria->compare('mail_transport_create_2',$this->mail_transport_create_2);
+		$criteria->compare('mail_kill_rate',$this->mail_kill_rate);
+		$criteria->compare('mail_deadline',$this->mail_deadline);
+		$criteria->compare('mail_before_deadline',$this->mail_before_deadline);
+		$criteria->compare('site_transport_create_1',$this->site_transport_create_1);
+		$criteria->compare('site_transport_create_2',$this->site_transport_create_2);
+		$criteria->compare('site_kill_rate',$this->site_kill_rate);
+		$criteria->compare('site_deadline',$this->site_deadline);
+		$criteria->compare('site_before_deadline',$this->site_before_deadline);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return UserField the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
