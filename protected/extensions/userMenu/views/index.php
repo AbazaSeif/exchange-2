3947d508696f3c7dbc8e0643100ca4d89da2d0b8
Tatsyana Krylova <krilova@lbr.ru>
2014-02-27 17:25:45 +0300

    url

--- protected/extensions/userMenu/views/index.php
+++ protected/extensions/userMenu/views/index.php
@@ -71,8 +71,8 @@ if(!Yii::app()->user->isGuest){
 $(document).ready(function(){
     <?php if(!Yii::app()->user->isGuest): ?>
     var userId = <?php echo $user->id ?>;
-    var socket = io.connect('http://exchange.lbr.ru:3000/');
-    //var socket = io.connect('http://localhost:3000/');
+    //var socket = io.connect('http://exchange.lbr.ru:3000/');
+    var socket = io.connect('http://localhost:3000/');
     
     socket.emit('init', userId, <?php echo Yii::app()->params['minNotyfy'] ?>);
     
