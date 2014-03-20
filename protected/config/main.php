172619456f68a689c72d5cc88bf9240fd0bb1151
Tatsyana Krylova <krilova@lbr.ru>
2014-03-20 13:54:14 +0300

    local url

--- protected/config/main.php
+++ protected/config/main.php
@@ -111,8 +111,8 @@ return array(
     // using Yii::app()->params['paramName']
     'params' => array(
         // this is used in contact page
-        //'host'=>'lbr.test',
-        'host'=>'lbr.ru',
+        'host'=>'lbr.test',
+        //'host'=>'lbr.ru',
         'meta_title' => 'Биржа перевозок',
         'meta_description' => 'перевозки, биржа перевозок, биржа ЛБР',
         'menu_admin' => array(
