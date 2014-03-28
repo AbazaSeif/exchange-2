14e19398f39f2c88923912181972a321b17b157d
Cheshenkov Zhenia <cheshenkov@lbr.ru>
2014-03-28 12:33:54 +0300

    Edit new

--- protected/modules/admin/views/default/index.php
+++ protected/modules/admin/views/default/index.php
@@ -4,20 +4,7 @@ $user = AuthUser::model()->findByPk(Yii::app()->user->_id);
 
 <h1>Добрый день, <?php $user->name;?></h1>
 <div class="info">
-    <ul class="info-list">
-        <li>
-            Перевозки
-        </li>
-        <li>
-            <span>Всего перевозок: <?php echo Transport::model()->count();?></span>
-        </li>
-        <li>
-            <span>Активных: <?php echo Transport::model()->count('status=1');?></span>
-        </li>
-        <li>
-            <span>Архивных: <?php echo Transport::model()->count('status=0');?></span>
-        </li>
-    </ul>
+    
     <ul class="info-list">
         <li>
             Перевозчики
@@ -39,6 +26,20 @@ $user = AuthUser::model()->findByPk(Yii::app()->user->_id);
         </li>
         <li>
             <span>Заблокированных: <?php echo User::model()->count('status='.User::USER_BLOCKED);?></span>
+        </li>
+    </ul>
+    <ul class="info-list">
+        <li>
+            Перевозки
+        </li>
+        <li>
+            <span>Всего перевозок: <?php echo Transport::model()->count();?></span>
+        </li>
+        <li>
+            <span>Активных: <?php echo Transport::model()->count('status=1');?></span>
+        </li>
+        <li>
+            <span>Архивных: <?php echo Transport::model()->count('status=0');?></span>
         </li>
     </ul>
 </div>
