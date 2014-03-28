4d5f8db691fbfadc9577d6afc05e86316a46d346
Cheshenkov Zhenia <cheshenkov@lbr.ru>
2014-03-28 11:51:24 +0300

    Add stats to admin panel

--- protected/modules/admin/views/default/index.php
+++ protected/modules/admin/views/default/index.php
@@ -1,18 +1,44 @@
 <?php
-$user = User::model()->findByPk(Yii::app()->user->_id);
+$user = AuthUser::model()->findByPk(Yii::app()->user->_id);
 ?>
 
 <h1>Добрый день, <?php $user->name;?></h1>
 <div class="info">
     <ul class="info-list">
         <li>
+            Перевозки
+        </li>
+        <li>
             <span>Всего перевозок: <?php echo Transport::model()->count();?></span>
         </li>
         <li>
-            <span>Активных перевозок: <?php echo Transport::model()->count('status=1');?></span>
+            <span>Активных: <?php echo Transport::model()->count('status=1');?></span>
         </li>
         <li>
-            <span>Активных перевозок: <?php echo Transport::model()->count('status=0');?></span>
+            <span>Ахивных: <?php echo Transport::model()->count('status=0');?></span>
+        </li>
+    </ul>
+    <ul class="info-list">
+        <li>
+            Перевозчики
+        </li>
+        <li>
+            <span>Всего перевозчиков: <?php echo User::model()->count('type=0');?></span>
+        </li>
+        <li>
+            <span>Активных: <?php echo Transport::model()->count('status='.User::USER_ACTIVE);?></span>
+        </li>
+        <li>
+            <span>Не подтвержденных: <?php echo Transport::model()->count('status='.User::USER_NOT_CONFIRMED);?></span>
+        </li>
+        <li>
+            <span>Предупрежденных: <?php echo Transport::model()->count('status='.User::USER_WARNING);?></span>
+        </li>
+        <li>
+            <span>Временно заблокированных: <?php echo Transport::model()->count('status='.User::USER_TEMPORARY_BLOCKED);?></span>
+        </li>
+        <li>
+            <span>Заблокированных: <?php echo Transport::model()->count('status='.User::USER_BLOCKED);?></span>
         </li>
     </ul>
 </div>
