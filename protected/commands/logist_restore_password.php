fda953817dbf064cc5181dd7d2fcdbf8c106e5c2
Tatsyana Krylova <krilova@lbr.ru>
2014-04-09 10:13:45 +0300

    templates for mail

---
+++ protected/commands/logist_restore_password.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=400, initial-scale=0.5" />
+	<meta content="telephone=no" name="format-detection" />
+	<title>Email Template</title>
+	<style type="text/css" media="screen">
+		/* Linked Styles */
+		body { padding:0 !important; margin:0 !important; display:block !important; background:#ededed; -webkit-text-size-adjust:none }
+
+		.footer a, .footer a:hover, .footer a:active { color: #ffffff; text-decoration: none; }
+                a, a:hover, a:active { color:#7b828b; text-decoration:underline }
+		/* Campaign Monitor wraps the text in editor in paragraphs. In order to preserve design spacing we remove the padding/margin */
+		p { padding:0 !important; margin:0 !important } 
+	</style>
+</head>
+<body class="body" style="padding:0 !important; margin:0 !important; display:block !important; background:#ededed; -webkit-text-size-adjust:none">
+
+<table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#ededed">
+    <tr>
+        <td align="center" valign="top">
+            <table width="624" border="0" cellspacing="0" cellpadding="0">
+                <!-- Top -->
+                <tr>
+                    <td class="top" style="color:#4a5461; font-family:Verdana; font-size:11px; line-height:15px; text-align:center">
+                        <img src="http://exchange.lbr.ru/images/test/empty.gif" width="1" height="20" style="height:20px; float: left" alt="" />
+                        <img src="http://exchange.lbr.ru/images/test/empty.gif" width="1" height="15" style="height:15px; float: left" alt="" />
+                    </td>
+                </tr>
+                <!-- END Top -->
+                <!-- Header -->
+                <tr>
+                    <td>
+                        <table style="margin: 0; padding: 0" width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td>
+                                    <img src="http://exchange.lbr.ru/images/test/header_top102.jpg" alt="" border="0" width="624" height="29" style="float: left"/>
+                                </td>
+                            </tr>
+                        </table>
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left;" width="1" bgcolor="#dfdfdf"></td>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left;" width="1" bgcolor="#c1c1c1"></td>
+                                <td bgcolor="#00abab">
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tr>
+                                            <td class="img" style="font-size:0pt; line-height:0pt; text-align:left; padding-left: 17px; padding-bottom: 10px"><img src="http://exchange.lbr.ru/images/test/header_logo99.jpg" style="border: 0; float: left" alt="" border="0" width="300" height="23" /></td>
+                                        </tr>
+                                    </table>
+                                </td>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" width="1" bgcolor="#c1c1c1"></td>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" width="1" bgcolor="#dfdfdf"></td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <!-- END Header -->
+                <!-- Content -->
+                <tr>
+                    <td>
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" width="1" bgcolor="#dfdfdf"></td>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" width="1" bgcolor="#c1c1c1"></td>
+                                <td bgcolor="#ffffff">
+                                    <!-- Main Content -->
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tr>
+                                            <td>
+                                                <img src="http://exchange.lbr.ru/images/test/content_top789.jpg" alt="" border="0" width="620" height="12" style="float: left"/>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tr>
+                                            <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" width="20"></td>
+                                            <td>
+                                                <img src="http://exchange.lbr.ru/images/test/empty.gif" width="1" height="15" style="height:15px; float: left" alt="" />
+                                                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                                    <tr>
+                                                        <td>
+                                                            <table width="100%" border="0" cellspacing="0" cellpadding="0" >
+                                                                <tr>
+                                                                    <td class="img" style="font-size:0pt; line-height:0pt; text-align:left; " valign="top" width="185">
+                                                                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                                                            <tr>
+                                                                                <td>
+                                                                                    <img src="http://exchange.lbr.ru/images/test/empty.gif" width="1" height="25" style="height:25px; float: left" alt="" />
+                                                                                </td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <td>
+                                                                                    <a href="http://exchange.lbr.ru/" target="_blank">
+                                                                                        <img src="http://exchange.lbr.ru/images/logo.png" alt="" border="0" width="179" height="66" style="float: left"/>
+                                                                                    </a>
+                                                                                </td>
+                                                                                <td>
+                                                                                    <img src="http://exchange.lbr.ru/images/test/empty.gif" width="20" height="1" style="width:20px" alt="" style="float: left"/>
+                                                                                </td>
+                                                                            </tr>
+                                                                        </table>
+                                                                    </td>
+                                                                    <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" valign="top" width="20"><img src="http://exchange.lbr.ru/images/test/img_right_shadow.jpg" alt="" border="0" width="8" height="131" style="float: left"/></td>
+                                                                    <td class="text" style="margin: 0; color:#a1a1a1; font-family:Verdana; font-size:12px; line-height:18px; text-align:left" valign="top">
+                                                                        <table width="100%" border="0" cellspacing="0" cellpadding="0" >
+                                                                            <tr>
+                                                                                <td style="padding-bottom: 10px; color:#000000; font-family:Verdana; font-size:14px; line-height:18px; text-align:left; font-weight:normal">
+                                                                                    Запрос на восстановление пароля.
+                                                                                </td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <td style="color:#000000; font-family:Verdana; font-size:14px; line-height:18px; text-align:left; font-weight:normal">
+                                                                                    Пользователь "ЧП Уткин В.Ю." запросил восстановление пароля, однако в базе не значится его email адрес. 
+                                                                                    <br/><br/>
+                                                                                    Просьба связаться с ним, узнать и внести в 1С либо на биржу email, и попросить его еще раз восстановить пароль. 
+                                                                                </td>
+                                                                            </tr>
+                                                                            
+                                                                        </table>
+                                                                    </td>
+                                                                </tr>
+                                                            </table>
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                            <td class="img" style="font-size:0pt; line-height:0pt; text-align:left; float: left" width="20"></td>
+                                        </tr>
+                                    </table>
+                                    <img src="http://exchange.lbr.ru/images/test/content_bottom.jpg" alt="" border="0" width="620" height="20" style="float: left"/>
+                                    <!-- END Main Content -->
+                                </td>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" width="1" bgcolor="#c1c1c1"></td>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" width="1" bgcolor="#dfdfdf"></td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <!-- END Content -->
+                <!-- Footer -->
+                <tr class="footer">
+                    <td>
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" width="1" bgcolor="#dfdfdf"></td>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" width="1" bgcolor="#b1b1b1"></td>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" width="1" bgcolor="#00abab"></td>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" width="1" bgcolor="#00abab"></td>
+                                <td bgcolor="#00abab">
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tr>
+                                            <td style="padding-left: 20px; padding-top: 14px; padding-bottom: 3px;"><span style="font-family:Verdana; font-size:11px; text-align:left;"><a href="http://exchange.lbr.ru/" target="_blank" style="text-decoration: none; color: #ffffff !important;">www.exchange.lbr.ru</a></span></td>
+                                        </tr>
+                                    </table>
+                                </td>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" width="1" bgcolor="#00abab"></td>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" width="1" bgcolor="#00abab"></td>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" width="1" bgcolor="#b1b1b1"></td>
+                                <td class="img" style="font-size:0pt; line-height:0pt; text-align:left" width="1" bgcolor="#dfdfdf"></td>
+                            </tr>
+                        </table>
+                        <img src="http://exchange.lbr.ru/images/test/footer_bottom102.jpg" alt="" border="0" width="624" height="29" style="padding-top: 0; float: left" />
+                    </td>
+                </tr>
+                <!-- END Footer -->
+                <!-- Bottom -->
+                <tr>
+                    <td class="bottom" style="color:#7b828b; font-family:Verdana; font-size:11px; line-height:20px; text-align:center">
+                        <table width="624" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td style="text-align:center;">
+                                    <a style="color:#7b828b; font-family:Verdana; font-size:11px; line-height:20px; text-align:center" href="http://exchange.lbr.ru/" target="_blank">support.ex@lbr.ru</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:center;">
+                                    <span style="color:#7b828b; font-family:Verdana; font-size:11px; line-height:20px; text-align:center">Copyright &copy; 2014 ООО "ЛБР-Агромаркет"</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="http://exchange.lbr.ru/images/test/empty.gif" width="1" height="50" style="height:50px; float: left" alt="" />
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <!-- END Bottom -->
+            </table>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
