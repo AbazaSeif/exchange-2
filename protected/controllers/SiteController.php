335dbf3904365631d46edf2cb0ed038e804382a6
Tatsyana Krylova <krilova@lbr.ru>
2014-04-16 17:00:44 +0300

    test mail

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -140,122 +140,36 @@ class SiteController extends Controller
     
     public function actionMailT()
     {
-        /*$headers["From"] = "tttanyattt@mail.ru";
-        $headers["To"] = "tttanyattt@mail.ru";
-        $headers["Subject"] = "User feedback";
-        
-        $mail = & Mail::factory('smtp', array('host' => 'mail.lbr.ru', 'port' => 25)); 
-        $mail->send('tttanyattt@mail.ru', $headers, 'jjjj'); */
-        $config['smtp_username'] = 'krilova@lbr.ru';  //Смените на имя своего почтового ящика.
-        $config['smtp_port']     = '25'; // Порт работы. Не меняйте, если не уверены.
-        $config['smtp_host']     = 'mail.lbr.ru';  //сервер для отправки почты
-        $config['smtp_password'] = 'pht76xu';  //Измените пароль
-        $config['smtp_debug']    = true;  //Если Вы хотите видеть сообщения ошибок, укажите true вместо false
-        $config['smtp_charset']  = 'windows-1251';  //кодировка сообщений. (или UTF-8, итд)
-        $config['smtp_from']     = 'test'; //Ваше имя - или имя Вашего сайта. Будет показывать при прочтении в поле "От кого"
-        
-        
-        $headers  = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-type: text/html; charset="windows-1251"' . "\r\n";
-        $headers .= 'From: tttanyattt@mail.ru'. "\r\n";
-        echo '111';
-        //$this->smtpmail('tttanyattt@mail.ru', 'Тема письма', 'Текст письма', $headers);
+        $body = '<h1>Test Mail</h1><p style="color: red">This is a test</p>';
+        $this->send_email('tttanyattt@mail.ru', 'Bob Smith <tttanyattt@mail.ru>', 'Test HTML message', $body);
     }
     
-    public function smtpmail($mail_to, $subject, $message, $headers='') {
-        global $config;
-        $SEND = "Date: ".date("D, d M Y H:i:s") . " UT\r\n";
-        $SEND .=    'Subject: =?'.$config['smtp_charset'].'?B?'.base64_encode($subject)."=?=\r\n";
-        if ($headers) $SEND .= $headers."\r\n\r\n";
-        else
-        {
-                $SEND .= "Reply-To: ".$config['smtp_username']."\r\n";
-                $SEND .= "MIME-Version: 1.0\r\n";
-                $SEND .= "Content-Type: text/plain; charset=\"".$config['smtp_charset']."\"\r\n";
-                $SEND .= "Content-Transfer-Encoding: 8bit\r\n";
-                $SEND .= "From: \"".$config['smtp_from']."\" <".$config['smtp_username'].">\r\n";
-                $SEND .= "To: $mail_to <$mail_to>\r\n";
-                $SEND .= "X-Priority: 3\r\n\r\n";
-        }
-        $SEND .=  $message."\r\n";
-         if( !$socket = fsockopen($config['smtp_host'], $config['smtp_port'], $errno, $errstr, 30) ) {
-            if ($config['smtp_debug']) echo $errno."&lt;br&gt;".$errstr;
-            return false;
-         }
+    public function send_email($to, $from, $subject, $body) {
+        require_once "Mail.php";
+        require_once "Mail/mime.php";    
 
-        if (!$this->server_parse($socket, "220", __LINE__)) return false;
+        $host = "mail.lbr.ru";
 
-        fputs($socket, "HELO " . $config['smtp_host'] . "\r\n");
-        if (!$this->server_parse($socket, "250", __LINE__)) {
-            if ($config['smtp_debug']) echo '<p>Не могу отправить HELO!</p>';
-            fclose($socket);
-            return false;
-        }
-        fputs($socket, "AUTH LOGIN\r\n");
-        if (!$this->server_parse($socket, "334", __LINE__)) {
-            if ($config['smtp_debug']) echo '<p>Не могу найти ответ на запрос авторизаци.</p>';
-            fclose($socket);
-            return false;
-        }
-        fputs($socket, base64_encode($config['smtp_username']) . "\r\n");
-        if (!$this->server_parse($socket, "334", __LINE__)) {
-            if ($config['smtp_debug']) echo '<p>Логин авторизации не был принят сервером!</p>';
-            fclose($socket);
-            return false;
-        }
-        fputs($socket, base64_encode($config['smtp_password']) . "\r\n");
-        if (!$this->server_parse($socket, "235", __LINE__)) {
-            if ($config['smtp_debug']) echo '<p>Пароль не был принят сервером как верный! Ошибка авторизации!</p>';
-            fclose($socket);
-            return false;
-        }
-        fputs($socket, "MAIL FROM: <".$config['smtp_username'].">\r\n");
-        if (!$this->server_parse($socket, "250", __LINE__)) {
-            if ($config['smtp_debug']) echo '<p>Не могу отправить комманду MAIL FROM: </p>';
-            fclose($socket);
-            return false;
-        }
-        fputs($socket, "RCPT TO: <" . $mail_to . ">\r\n");
+        $headers = array(
+                'From' => $from,
+                'To' => $to,
+                'Subject' => $subject
+        );
 
-        if (!$this->server_parse($socket, "250", __LINE__)) {
-            if ($config['smtp_debug']) echo '<p>Не могу отправить комманду RCPT TO: </p>';
-            fclose($socket);
-            return false;
-        }
-        fputs($socket, "DATA\r\n");
+        $mime = new Mail_mime();
+        $mime->setHTMLBody($body);
 
-        if (!$this->server_parse($socket, "354", __LINE__)) {
-            if ($config['smtp_debug']) echo '<p>Не могу отправить комманду DATA</p>';
-            fclose($socket);
-            return false;
-        }
-        fputs($socket, $SEND."\r\n.\r\n");
+        $body = $mime->get();
+        $headers = $mime->headers($headers);
 
-        if (!$this->server_parse($socket, "250", __LINE__)) {
-            if ($config['smtp_debug']) echo '<p>Не смог отправить тело письма. Письмо не было отправленно!</p>';
-            fclose($socket);
-            return false;
-        }
-        fputs($socket, "QUIT\r\n");
-        fclose($socket);
-        return TRUE;
-    }
+        $smtp = Mail::factory('smtp', array ('host' => $host, 'port' => 25));
+        $mail = $smtp->send($to, $headers, $body);
 
-    public function server_parse($socket, $response, $line = __LINE__) {
-        global $config;
-        while (@substr($server_response, 3, 1) != ' ') {
-            if (!($server_response = fgets($socket, 256))) {
-                if ($config['smtp_debug']) echo '<p>Проблемы с отправкой почты!</p>'.$response.'<br>'.$line.'<br>';
-                //if ($config['smtp_debug']) echo '<p>Проблемы с отправкой почты!</p>'.$response.'222'.$line.'1111';
+        if (PEAR::isError($mail)) {
                 return false;
-            }
+        } else {
+                return true; 
         }
-        if (!(substr($server_response, 0, 3) == $response)) {
-            if ($config['smtp_debug']) echo '<p>Проблемы с отправкой почты!</p>'.$response.'<br>'.$line.'<br>';
-            //if ($config['smtp_debug']) echo '<p>Проблемы с отправкой почты!</p>'.$response.'333'.$line.'44';
-            return false;
-        }
-        return true;
     }
     /*************************************************************/
     public function actionRegistration()
