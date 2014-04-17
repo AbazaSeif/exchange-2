00adbf8d83a7c638f137b30f9e99ab6425267d1c
Tatsyana Krylova <krilova@lbr.ru>
2014-04-17 13:21:33 +0300

    test mail

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -1,12 +1,15 @@
 <?php
-require_once("/usr/share/pear/Mail.php");
-require_once("/usr/share/pear/Mail/mime.php");  
+//require_once("/usr/share/pear/Mail.php");
+//require_once("/usr/share/pear/Mail/mime.php");  
 
 class TestController extends Controller
 {
     public function actionIndex()
     {
-       echo 2222; exit; 
+       echo Yii::app()->basePath; 
+       echo '<br/>';
+       echo dirname(__FILE__).'/../../../../../../';
+       exit; 
     }
 }
 
