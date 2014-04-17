d703d060daa94e540acee1deee072982890a7205
Tatsyana Krylova <krilova@lbr.ru>
2014-04-17 15:33:47 +0300

    test mail

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -11,11 +11,11 @@ class TestController extends Controller
        echo dirname(__FILE__); 
        echo '<br/>';
        //if(file_exists(dirname(__FILE__).'/../../../../../../../../usr/share/pear/Mail.php'))
-       $path = dirname(__FILE__).'/../../yii/';
+       $path = dirname(__FILE__).'/../../yii';
        if(file_exists($path))
       // if(file_exists('/usr/share/pear/Mail.php'))
            echo '1 - ' . $path;
-       else echo 0;
+       else echo '0 - ' . $path;
        //echo dirname(__FILE__).'/../../../../../../../';
        exit; 
     }
