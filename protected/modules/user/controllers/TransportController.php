4469b60491857e8bf51f72fa91dbcd80d73ae25a
Tatsyana Krylova <krilova@lbr.ru>
2014-07-22 12:56:25 +0300

    uncomment

--- protected/modules/user/controllers/TransportController.php
+++ protected/modules/user/controllers/TransportController.php
@@ -431,7 +431,7 @@ class TransportController extends Controller
         } else if($user->status == User::USER_WARNING || $user->status == User::USER_ACTIVE) {
             $allow = true;
         } else $status = 'Ваш статус "'.User::statusLabel($user->status).'".';
-        /*if($allow) {
+        if($allow) {
             $transport = Transport::model()->findByPk($id);
             $end = strtotime($transport->date_close);
             $now = strtotime(date('Y-m-d H:i:s'));
@@ -440,7 +440,7 @@ class TransportController extends Controller
                 $allow = false;
                 $status = 'вышло время отведенное для ставок.';
             }
-        }*/
+        }
         $array = array('status'=>$status, 'allow' => $allow);
         echo json_encode($array);
     }
