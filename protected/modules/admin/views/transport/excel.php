fbdcd1cc62a45efc1dfcb5d65ea8f13739506af8
Tatsyana Krylova <krilova@lbr.ru>
2014-09-16 15:00:56 +0300

    excel

---
+++ protected/modules/admin/views/transport/excel.php
@@ -0,0 +1,65 @@
+<?php if($model!=null):?>
+<table border="1">
+    <tr>
+        <th>Id 1C</th>
+        <th>Время закрытия заявки</th>
+        <th>Место загрузки</th>
+        <th>Место разгрузки</th>
+        <th>Кол-во ставок</th>
+        <th>Кол-во фирм</th>
+        <th>Фирма победитель</th>
+        <th>Лучшая ставка</th>
+        <th>Начальная ставка</th>
+        <th>Валюта</th>
+    </tr>
+    <?php foreach($model as $transport):
+        $currency = ' €';
+        $showRate = $withNds = '';
+        if (!$transport->currency) {
+           $currency = ' руб.';
+        } else if($transport->currency == 1) {
+           $currency = ' $';
+        } 
+        $rateCount = Rate::model()->countByAttributes(array(
+            'transport_id'=> $transport->id
+        ));
+        $users = Yii::app()->db->createCommand(array(
+            'select'   => 'user_id',
+            'distinct' => 'true',
+            'from'     => 'rate',
+            'where'    => 'transport_id = ' . $transport->id,
+        ))->queryAll();
+        $userCount = count($users);
+        
+        $rate = Rate::model()->findByPk($transport->rate_id);
+        $ferryman = User::model()->findByPk($rate->user_id);
+        $ferrymanField = UserField::model()->findByAttributes(array('user_id'=>$rate->user_id));
+        if ($rate->price) {
+            $showRate = floor($rate->price);
+            if($ferrymanField->with_nds && $transport->type == Transport::RUS_TRANSPORT) {
+                $price = ceil($rate->price + $rate->price * Yii::app()->params['nds']);
+                if($price%10 != 0) $price -= $price%10;
+                $withNds .= ' (c НДС: '. $price.')';
+            }
+        }
+        if($withNds) $showRate = $showRate.' '.$withNds;
+        
+    ?>
+    <tr>
+        <td><?php echo $transport->t_id; ?></td>
+        <td><?php echo date('Y-m-d H:i', strtotime($transport->date_close)); ?></td>
+        <td><?php echo $transport->location_from; ?></td>
+        <td><?php echo $transport->location_to; ?></td>
+        <td><?php echo $rateCount; ?></td>
+        <td><?php echo $userCount; ?></td>
+        <td><?php echo ($ferryman->company) ? $ferryman->company : 'Нет ставок' ?></td>
+        <td><?php echo $showRate;  ?></td>
+        <td><?php echo $transport->start_rate; ?></td>
+        <td><?php echo $currency; ?></td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+<?php endif; ?>
+
+
+
