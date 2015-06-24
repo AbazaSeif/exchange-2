cbd6801d7a291997f9ba2527eb3394447962f665
Tatsyana Krylova <krilova@lbr.ru>
2015-06-24 10:21:46 +0300

    comment google analitics

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -28,7 +28,7 @@
             Yii::app()->clientScript->registerScriptFile('/js/front/frontend.js');
             Yii::app()->clientScript->registerScriptFile('/js/front/OnlineEvent.js');
             //google analitics
-            Yii::app()->clientScript->registerScriptFile('/js/lbr.google.analytics.js');
+            //Yii::app()->clientScript->registerScriptFile('/js/lbr.google.analytics.js');
         ?>
     </head>
     <body>
