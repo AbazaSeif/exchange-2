f2a878896db75090232ea1039576b6ed73bc9319
Tatsyana Krylova <krilova@lbr.ru>
2015-10-29 15:35:11 +0300

    update

--- protected/commands/CronCommand.php
+++ protected/commands/CronCommand.php
@@ -25,6 +25,7 @@ class CronCommand extends CConsoleCommand
         ;
         
         foreach($transports as $transport){
+            Yii::log('error date ---- '.$transport['id'], 'info');
             Transport::model()->updateByPk($transport['id'], array('status' => 0));
         }
     }
