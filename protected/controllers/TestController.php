8bdb59a90307e16c7ce3035d50eab43c46a495de
Tatsyana Krylova <krilova@lbr.ru>
2016-08-10 11:10:33 +0300

    update

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -3,7 +3,6 @@ class TestController extends Controller
 {
     public function actionIndex()
     {
-        TransportInterPoint::model()->deleteAll('t_id=:tid', array(':tid' => 394));
-        echo 394;
+        
     }
 }
